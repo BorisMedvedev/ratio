1b02738810c8f8e268cc7c3f04a451607b0ef17a
Your Name <you@example.com>
2022-04-24 12:08:45 +0300

    format

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-  $('.slider').slick({
-    prevArrow: '<button class="slider-btn  slider-btnprev"><img src="./images/arrow-prev.svg" alt="" width="40" height="14"></button>',
-    nextArrow: '<button class="slider-btn  slider-btnnext"><img src="./images/arrow-next.svg" alt=""></button>',
+  $(".slider").slick({
+    prevArrow:
+      '<button class="slider-btn  slider-btnprev"><img src="./images/arrow-prev.svg" alt="" width="40" height="14"></button>',
+    nextArrow:
+      '<button class="slider-btn  slider-btnnext"><img src="./images/arrow-next.svg" alt="" width="20" height="14"></button>',
     dots: true,
     infinite: true,
     loop: false,
@@ -10,45 +12,40 @@ $(document).ready(function () {
     slidesToShow: 1,
     // centerMode: true,
     variableWidth: true,
-    responsive: [{
-      breakpoint: 968,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: false,
-        arrows: false
-
-      }
-    }
-
-    ]
+    responsive: [
+      {
+        breakpoint: 968,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          arrows: false,
+        },
+      },
+    ],
   });
 
-
-  $('.hamburger').click(function () {
-    $(this).toggleClass('open');
-    $('.header-nav').toggleClass('header-nav--open');
+  $(".hamburger").click(function () {
+    $(this).toggleClass("open");
+    $(".header-nav").toggleClass("header-nav--open");
   });
-  $('.header-nav__link').click(function () {
-    $('.hamburger').removeClass('open');
-    $('.header-nav').removeClass('header-nav--open');
-
+  $(".header-nav__link").click(function () {
+    $(".hamburger").removeClass("open");
+    $(".header-nav").removeClass("header-nav--open");
   });
-
-
 });
 
-const anchors = document.querySelectorAll('a')
+const anchors = document.querySelectorAll("a");
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    const blockID = anchor.getAttribute('href');
+    const blockID = anchor.getAttribute("href");
 
     document.querySelector(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+      behavior: "smooth",
+      block: "start",
     });
   });
 }
